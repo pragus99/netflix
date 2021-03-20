f9a2b22295382641eedce460d719cdeb424b618a
prayogo bagus suntoro <prayogo0399@gmail.com>
2021-03-20 17:39:48 +0700

    js for add and remove class for tab. this final.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
 
-// select tab content item
+// memilih isi content tab
 function selectItem(e) {
     removeBorder();
+    removeShow();
     //add border to current tab
     this.classList.add('tab-border');
+    // ambil isi content dari DOM dengan ID
+    console.log(this.id)
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    //menambah class show
+    tabContentItem.classList.add('show');
 }
 
 function removeBorder() {
-    tabItems.forEach(item => item.classList.remove('tab-border'))
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
 }
 
 // listen for tab click
